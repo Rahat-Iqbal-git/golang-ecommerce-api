136ee9890385d33e24b3b50816fd773b6b221195
rahat.iqbal <rahat.iqbal@technonext.com>
2026-05-15 01:55:09 +0600

    add env for running postgre

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"log"
 
+	// "github.com/joho/godotenv"
 	"github.com/rahat-iqbal/ecommerce-api/internal/config"
 	"github.com/rahat-iqbal/ecommerce-api/internal/routes"
 )
 
 func main() {
+	// _ = godotenv.Load() // loads .env if present; ignored in production where env vars are set directly
+
 	cfg := config.Load()
 
 	router := routes.Setup(cfg)
